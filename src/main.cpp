c6778a1e27de506f2f17e587c07f3d5b041c62cc
root <root@johnny-VirtualBox.(none)>
2018-01-11 13:36:37 +0800

    fix unstable linear regression

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 using namespace cv;
 using namespace std;
 
+
+//#define test_4_debug 0 
+//#define test_simple_point1 0
+
+
   
 int main(int argc, char *argv[])  
 {  
@@ -12,8 +17,9 @@ int main(int argc, char *argv[])
         IplImage* img0;  
         IplImage* img1;  
         IplImage* img2;  
+        IplImage* img3; 
         std::string pic_name="test";
-
+        std::string pic_new="new";
 
 
    	cv::Mat image = cv::imread("test-6.jpg", CV_LOAD_IMAGE_GRAYSCALE);
@@ -36,7 +42,11 @@ int main(int argc, char *argv[])
 
     	cv::imshow("localThreshold", local);
 
+
+//        #ifdef test_4_debug 
         img = cvLoadImage("local.jpg");//默认初始图像放在工程文件下  
+//        #endif 
+
         
 	if (NULL == img)  
             return 0;  
@@ -49,10 +59,11 @@ int main(int argc, char *argv[])
         img1 = cvCreateImage(cvGetSize(img),IPL_DEPTH_8U,1);//申请一段内存  
         cvCopy(img0, img1, NULL);//数据复制，若直接赋值相当指针指向同一地址会对原本img0操作  
         img2 = cvCreateImage(cvGetSize(img),IPL_DEPTH_8U,1);//申请一段内存
-
+        img3 = cvCreateImage(cvGetSize(img),IPL_DEPTH_8U,1);//申请一段内存
 //二值化操作  
         int height = img1->height;  
         int width = img1->width;  
+//        int step  = img1->width;
         int step = img1->widthStep;  
         int channels = img1->nChannels; 
         bool on=true;
@@ -60,7 +71,7 @@ int main(int argc, char *argv[])
         cout<<"size : height="<<img1->height<<"  width="<<img1->width<<" widthStep="<<img1->widthStep<<" nchannels="<<img1->nChannels<<std::endl; 
 
         uchar *data = (uchar*)img1->imageData;  
-  
+        uchar *data2 = (uchar*)img2->imageData; 
 
         for(int i=0;i != height; ++ i)  
         {  
@@ -78,63 +89,131 @@ int main(int argc, char *argv[])
 //                   }
                  }
                  else  
+                 {
+                  #ifdef test_simple_point1
+                  data[i*step+j*channels+k]=0;
+                  #else
                   data[i*step+j*channels+k]=255;//255-data[i*step+j*channels+k];  
+                  #endif
+                 }
              }  
          }  
         } 
 
 // removing noise
 
-        
+//data[4*step+118]=255;
+
+#ifdef test_simple_point1
+data[4*step+5]=255;
+data[8*step+19]=255;
+data[9*step+13]=255;
+data[9*step+25]=255;
+data[10*step+6]=255;
+data[14*step+20]=255;
+data[16*step+27]=255;
+data[19*step+22]=255;
+
+data[19*step+80]=255;
+//data[15*step+111]=255;
+#endif
 
 
+/*
+data[21*step+9]=255;
+data[26*step+7]=255;
+data[6*step+37]=255;
+data[5*step+48]=255;
+data[14*step+20]=255;
+data[19*step+22]=255;
+data[9*step+25]=255;
+data[16*step+27]=255;
+
+data[3*step+80]=255;
+*/
 
 
+//data[7*step+122]=255;
+//data2[7*step+122]=255;
+
+
+//        data2[1*step+0]=255;       
+//        data2[1*step+1]=255;
+//        data2[1*step+2]=255;
+
+#ifdef test_4_debug
+
+        data2[23*step+84]=0;
+        data2[23*step+85]=0;
+        data2[4*step+106]=0;
+        data2[3*step+106]=0;
+        data2[3*step+105]=0;
+ //       data2[3*step+104]=0;
+        data2[2*step+104]=0;
+
+        data[23*step+84]=0;
+        data[23*step+85]=0;
+        data[4*step+106]=0;
+        data[3*step+106]=0; 
+        data[3*step+105]=0;
+ //       data[3*step+104]=0;
+        data[2*step+104]=0;
+
+//        data[4*step+132]=255;
+//        data2[4*step+132]=255;
+//        data[5*step+132]=255;
+//        data2[5*step+132]=255;
+
+#endif 
+
+/*
+       data[4*step+87]=255;
+       data[5*step+87]=255;
+       data[7*step+87]=255;
+       data[6*step+87]=255;
+
+       data2[4*step+87]=255;
+       data2[5*step+87]=255;
+       data2[6*step+87]=255;
+       data2[7*step+87]=255;
+
+*/
 
         cvCopy(img1, img2, NULL);
-
-        bool init=true;
-        unsigned long int xl=0,yl=0,num=0; 
-        double b=0 ,a=0;    
-        double d=0, dl=0, di=0,dx=0,dy=0;
-        double new_d=0 ;
-        
+        cvCopy(img1, img3, NULL);
 
         int height_start=  0, height_end= img1->height ; 
         int width_start=   0 , width_end=  img1->width;
 
+        double xl=0,yl=0 ;
 
-        for(int itera=0;itera<8;itera++)        
+        int num=0; 
+
+        float a=0,b=0;          
+
+	float  dl=0,d=0;
+
+        float new_d=0;
+
+        for(int itera=0;itera<5;itera++)        
         {
 
         num=0;
         xl=0;
         yl=0;                 
-        for(int y=height_start;y !=height_end; ++y)
+        for(int x=height_start;x !=height_end; ++x)
         {
-           for(int x=width_start;x !=width_end; ++x)  
+           for(int y=width_start;y !=width_end; ++y)  
            {
                
-//               printf("--- test x=%d,y=%d,data=%d \n",x,y,data[y*step+x]);
-               if(255==data[y*step+x])    
+//              printf("--- test x=%d,y=%d,data=%d \n",x,y,data2[x*step+y]);
+               if(255==data[x*step+y])    
                {
-                   if(init==false)
-                   {
-                       dx=abs(b*y-x+a);
-                       dy=sqrt(b*b+1);
-                       d=dx/dy ;
-//                       printf("--test-- d=%g \n",d);
-                   }
-                   if((init==true)||(d<=new_d))
-                   {
 
                        num++;
                        xl+=x;
                        yl+=y;
-//                       printf(" --- line-point x=%d y=%d xl=%d yl=%d num=%d \n",x,y,xl,yl,num);  
-                   
-                   }
-
+                       printf(" --- line-point x=%d y=%d xl=%g yl=%g num=%d \n",x,y,xl,yl,num);  
                }
            }
         }
@@ -147,116 +226,111 @@ int main(int argc, char *argv[])
         xi=xl/num ;
         yi=yl/num ; 
 
-        printf("--- c-point xi=%f,yi=%f \n",xi,yi); 
+        printf("--- c-point xi=%g,yi=%g num=%d \n",xi,yi,num); 
 
 
         double ax=0,bx=0;
        
-        for(int y=height_start;y !=height_end; ++y)
+        for(int x=height_start;x !=height_end; ++x)
         {
-           for(int x=width_start;x !=width_end; ++x)
+           for(int y=width_start;y !=width_end; ++y)
            {
-               if(255==data[y*step+x])
+               if(255==data[x*step+y])
                {
-                   if(init==false)
-                   {              
-                       dx=abs(b*y-x+a);
-                       dy=sqrt(b*b+1);
-                       d=dx/dy ;
-                   }
-
-                   if((init==true)||(d<=new_d))
-                   {
                         ax+=(x-xi)*x ;
                         bx+=(y-yi)*x ;
-//                        printf(" --- line-b x=%d y=%d aa=%f bb=%f  \n",x,y,(x-xi)*x,(y-yi)*x);
-//                        printf(" --- line-b x=%d y=%d ax=%f bx=%f  \n",x,y,ax,bx);
-                   }
+                        printf(" --- line-b x=%d y=%d aa=%f bb=%f  \n",x,y,(x-xi)*x,(y-yi)*x);
+                        printf(" --- line-b x=%d y=%d ax=%f bx=%f  \n",x,y,ax,bx);
                }
            }
         }
 
 
-        b=bx/ax;                 
+       b=bx/ax;                 
         
-        a=yi-b*xi;
+       a=yi-b*xi;
 
-        printf(" ---- linx y= %f+%fx \n", a,b);
+       printf(" ---- linx y= %f+%fx \n", a,b); 
 
-                  
-        for(int y=height_start;y !=height_end; ++y)
+       dl=0; 
+
+       unsigned int dl_num=0;
+       float dx=0,dy=0,di;
+
+       for(int x=height_start;x !=height_end; ++x)
         {
-           for(int x=width_start;x !=width_end; ++x)
+           for(int y=width_start;y !=width_end; ++y)
            {
-               if(255==data[y*step+x])
+               if(255==data[x*step+y])
                {
-                   if(init==false)
-                   {
-                       dx=abs(b*y-x+a);
-                       dy=sqrt(b*b+1);
-                       d=dx/dy ;
-                   }
-
-                   if((init==true)||(d<=new_d))
-                   {
-                       dx=abs(b*y-x+a);
+                       dl_num++;
+                       dx=abs(b*x-y+a);
                        dy=sqrt(b*b+1);
                        dl+=dx/dy ;
-  //                     printf("--- x=%d,y=%d  distance=%g \n",x,y,dx/dy);
-                   }
+//                       printf("--- x=%d,y=%d  distance=%g \n",x,y,dx/dy);
                }
             }
- 
+
         }
+         
+       di=dl/dl_num;
 
-        di=d/num;
+       printf("--- di=%g dl=%g dl_num=%d \n",di,dl,dl_num);
 
-        printf("--- di=%f  \n",di);
-        
-        float si=0,si2=0;
- 
-        for(int y=height_start;y !=height_end; ++y)
-        {
-           for(int x=width_start;x !=width_end; ++x)
+
+       unsigned int test_x=20,test_y=23; 
+       float test_distance=0;
+
+
+ //      dx=abs(b*test_x-test_y+a);
+       dx=b*test_x-test_y+a; 
+       dy=sqrt(b*b+1);
+       test_distance=dx/dy ;
+      // data[15*step+78]=255;
+
+       printf("---- test distance\n");
+       printf("---- line y= %f+%fx \n", a,b);
+       printf("---- point x=%d,y=%d test_d=%g \n",test_x,test_y,test_distance);
+     
+
+      float si=0,si2=0;
+      unsigned s_num=0;
+
+      for(int x=height_start;x !=height_end; ++x)
+      {
+           for(int y=width_start;y !=width_end; ++y)
            {
-               if(255==data[y*step+x])
-               {   
-                   if(init==false)
+                   if(255==data[x*step+y])
                    {
-                       dx=abs(b*y-x+a);
-                       dy=sqrt(b*b+1);
-                       d=dx/dy ;
-                   }
-
-                   if((init==true)||(d<=new_d))
-                   {
-                       dx=abs(b*y-x+a);
+                       dx=b*x-y+a;
                        dy=sqrt(b*b+1);
                        d=dx/dy ;
 //                       printf("--test-- d=%g \n",d);
-                       si+=(d-di)*(d-di); 
+                       si+=(d-di)*(d-di);
+//                       si+=abs(d-di);
                    }
-               }
            }
 
-        }
+       }
 
        si2=si/(num-1);
 
        printf("--- si=%g  si2=%g  \n",si,si2);
 
-
-
        new_d=sqrt(si2);
 
-       printf("--- new_d %g \n",new_d);
+//       new_d=si2;
 
-       init=false;        
+       printf("--- new_d %g \n",new_d); 
 
-      
-       for(int y=height_start;y !=height_end; y++)
+
+       cvCopy(img3, img2, NULL);       
+     
+
+       //data2[15*step+78]=255; 
+       for(int x=height_start;x !=height_end; x++)
         {
-           for(int x=width_start;x !=width_end; x++)
+           for(int y=width_start;y !=width_end; y++)
            {
                if(int(a+b*x)==y)
                {
@@ -267,17 +341,40 @@ int main(int argc, char *argv[])
 //                       data[y*step+x-1]=255;
 //                       data[y*step+x-2]=255;
 //                   }
-                   data[y*step+x]=0;
+                   #ifdef  test_simple_point1
+                   data2[x*step+y]=255;
+                   #else 
+                   data2[x*step+y]=255;
+                   #endif 
                    
                }
             }
         }
 
-        pic_name+="1" ;
-        cvNamedWindow(pic_name.c_str(), 0 );
-        cvShowImage(pic_name.c_str(), img1);
+        cvCopy(img3, img1, NULL);
 
-        cvCopy(img2, img1, NULL); 
+
+     //   double dx=0,dy=0;
+        for(int x=height_start;x !=height_end; x++)
+        {
+            for(int y=width_start;y !=width_end; y++)
+            {     
+                dx=abs(b*x-y+a);
+                dy=sqrt(b*b+1);
+                d=dx/dy ;      
+                if(d>new_d)
+                data[x*step+y]=0;
+            }
+        }
+
+        pic_name+="1" ;
+        pic_new+="1";
+        cvNamedWindow(pic_name.c_str(), 0 );
+        cvShowImage(pic_name.c_str(), img2);
+        cvNamedWindow(pic_new.c_str(), 0 );
+        cvShowImage(pic_new.c_str(), img1);
+
+//        cvCopy(img1, img2, NULL); 
         }       
 
 //创建窗口、显示图像、销毁图像、释放图像  
@@ -287,7 +384,7 @@ int main(int argc, char *argv[])
         cvWaitKey(0);  
   
 //        cvDestroyWindow( "test1" );  
-        cvDestroyWindow( "test" );  
+        cvDestroyWindow( "test1" );  
   
         cvReleaseImage( &img0 );  
         cvReleaseImage( &img1 );  
